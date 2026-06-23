0029473d684ebe5ab233423724a3fa24f5e94820
Vamsha vardhan <chintha.vamshavardhan50@gmail.com>
2026-06-24 00:44:05 +0530

    Revert "build: configure next config to use native serverless build on vercel and trailingSlash for static exports"
    This reverts commit cf532b82706545e5474adda98178b9da25a79ab1.

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,12 +3,9 @@ import { createMDX } from 'fumadocs-mdx/next';
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
-const isVercel = process.env.VERCEL === '1';
-
 const config = {
   reactStrictMode: true,
-  output: isVercel ? undefined : 'export',
-  trailingSlash: true,
+  output: 'export',
 };
 
 
